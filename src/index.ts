e25fcea7025183800f6f8992cb694c5ed7d04c28
podpison <au25557@yandex.ru>
2024-10-23 14:23:01 +0500

    temp: disable DB

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,27 @@
 import "dotenv/config";
 import WOtD from "./core.ts";
 import Table from "./table.ts";
-import { WordsAPI } from "./api";
+// import { WordsAPI } from "./api";
 
 const vocabularyWOtD = new WOtD();
 await vocabularyWOtD.init("https://www.vocabulary.com/word-of-the-day/");
 
-const wordsAPI = new WordsAPI();
+// const wordsAPI = new WordsAPI();
 
-const existanceCode = await wordsAPI.doesExist(vocabularyWOtD.word);
+// const existanceCode = await wordsAPI.doesExist(vocabularyWOtD.word);
 
-if (existanceCode !== 200) {
-	const result = await wordsAPI.set(vocabularyWOtD.word);
+// if (existanceCode !== 200) {
+// 	const result = await wordsAPI.set(vocabularyWOtD.word);
 	
-	if (result !== 200) {
-		//TODO: to helpers
-		console.log("Something went wrong");
-	}
-}
+// 	if (result !== 200) {
+// 		//TODO: to helpers
+// 		console.log("Something went wrong");
+// 	}
+// }
 
 //TODO: move offset login to a seprate file
-const words = await wordsAPI.getAll();
+// const words = await wordsAPI.getAll();
 
-const vocabularyTable = new Table(words);
+const vocabularyTable = new Table([vocabularyWOtD.word]);
 
 vocabularyTable.print();
